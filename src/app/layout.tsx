62b7eb873f245bac7b809ecd4a423e91a53cc138
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-31 19:15:07 +0530

    Use the clerk provider
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '~/styles/globals.css'
 
+import { ClerkProvider } from '@clerk/nextjs'
 import { GeistSans } from 'geist/font/sans'
 import type { Metadata, Viewport } from 'next'
 import BaseLayout from '~/components/BaseLayout'
@@ -19,10 +20,12 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <BaseLayout>{children}</BaseLayout>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>
+          <BaseLayout>{children}</BaseLayout>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
