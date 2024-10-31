aa0b8d519bba5e0cfd6c36b456dabbf80e0aae2d
JagTheFriend <JagTheFriend12@gmail.com>
2024-10-31 19:13:44 +0530

    No need for extra step
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -8,14 +8,10 @@ export const authenticatedAction = createSafeActionClient({
   handleServerError: (_error) => {
     return DEFAULT_SERVER_ERROR_MESSAGE
   },
+}).use(async ({ next }) => {
+  const currentUserData = await auth()
+  if (!currentUserData.userId) {
+    throw new Error('Not authenticated')
+  }
+  return next({ ctx: { currentUserData } })
 })
-  .use(async ({ next }) => {
-    const currentUserData = await auth()
-    return next({ ctx: { currentUserData } })
-  })
-  .use(async ({ next, ctx }) => {
-    if (!ctx.currentUserData) {
-      throw new Error('Not authenticated')
-    }
-    return next({ ctx })
-  })
