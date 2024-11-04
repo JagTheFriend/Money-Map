2b5bc76e4bd1ec7bb9aed8beca3706d9ce0417a0
JagTheFriend <JagTheFriend12@gmail.com>
2024-11-04 08:06:57 +0530

    Redirect to auth page
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/_components/AuthButtons.tsx
+++ src/app/_components/AuthButtons.tsx
@@ -12,7 +12,7 @@ export default function AuthButtons() {
         transition={{ duration: 1 }}
         className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200 flex items-center"
       >
-        <Link href="#">Get Started</Link>
+        <Link href="/auth">Get Started</Link>
       </motion.div>
       <motion.div
         initial={{ x: '100%' }}
@@ -20,7 +20,7 @@ export default function AuthButtons() {
         transition={{ duration: 1 }}
         className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
       >
-        <Link href="#">Login</Link>
+        <Link href="/auth?type='login'">Login</Link>
       </motion.div>
     </section>
   )
