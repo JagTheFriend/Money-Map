e342739b45f8387584b2a80607cfdbbc8d4b6f45
JagTheFriend <JagTheFriend12@gmail.com>
2024-11-05 08:27:42 +0530

    Display the landing page when user is not authenticated
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
+import { SignedOut } from '@clerk/nextjs'
 import AuthButtons from './_components/AuthButtons'
 import Features from './_components/Features'
 import IntroBanner from './_components/IntroBanner'
 
 export default function HomePage() {
   return (
-    <>
+    <SignedOut>
       <IntroBanner />
       <Features />
       <AuthButtons />
-    </>
+    </SignedOut>
   )
 }
