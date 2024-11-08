fcfb81fbe419a8eeab8cc0af98ffd4ef2e1a2f56
JagTheFriend <JagTheFriend12@gmail.com>
2024-11-08 21:25:02 +0530

    Redirect user when different date is selected
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/_components/ShowCurrentlySelectedYear.tsx
+++ src/app/_components/ShowCurrentlySelectedYear.tsx
@@ -1,21 +1,29 @@
 'use client'
 
-import { useState } from 'react'
 import DatePicker from 'react-date-picker'
 
+import { redirect } from 'next/navigation'
 import 'react-calendar/dist/Calendar.css'
 import 'react-date-picker/dist/DatePicker.css'
 
 type ValuePiece = Date | null
-
 type Value = ValuePiece | [ValuePiece, ValuePiece]
 
 export default function ShowCurrentlySelectedYear({ year }: { year: string }) {
-  const [value, onChange] = useState<Value>(new Date(parseInt(year), 0, 1))
-
   return (
-    <section>
-      <DatePicker onChange={onChange} value={value} maxDetail="year" />
+    <section className="text-xl flex flex-row justify-center gap-2 m-2">
+      Year:
+      <DatePicker
+        onChange={(e: Value) => {
+          const date = e?.toString() ?? ''
+          const year = new Date(date).getFullYear()
+          redirect(`?year=${year}`)
+        }}
+        value={new Date(year)}
+        maxDetail="decade"
+        clearIcon={false}
+        calendarIcon={false}
+      />
     </section>
   )
 }
