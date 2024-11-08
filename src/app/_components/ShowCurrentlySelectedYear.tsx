06a15286af89416825a13eccef943ad40b255ba6
JagTheFriend <JagTheFriend12@gmail.com>
2024-11-08 08:42:27 +0530

    Working on year picker
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/_components/ShowCurrentlySelectedYear.tsx
+++ src/app/_components/ShowCurrentlySelectedYear.tsx
@@ -1,5 +1,18 @@
 'use client'
 
+import { useState } from 'react'
+import DatePicker from 'react-date-picker'
+
+type ValuePiece = Date | null
+
+type Value = ValuePiece | [ValuePiece, ValuePiece]
+
 export default function ShowCurrentlySelectedYear({ year }: { year: string }) {
-  return <div>Currently Selected Year: {year}</div>
+  const [value, onChange] = useState<Value>(new Date())
+
+  return (
+    <section>
+      <DatePicker onChange={onChange} value={value} />
+    </section>
+  )
 }
