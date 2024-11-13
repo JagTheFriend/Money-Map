f6548c2628d8892bfb6820db737f09fa1f6c7c04
JagTheFriend <JagTheFriend12@gmail.com>
2024-11-13 08:47:17 +0530

    Working on a way to display monthly expense
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/_components/MonthLineChart.tsx
+++ src/app/_components/MonthLineChart.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { IconArrowLeft, IconArrowRight } from '@tabler/icons-react'
 import {
   CartesianGrid,
   Legend,
@@ -13,15 +14,29 @@ import { data } from './data'
 
 export const MonthLineChart = () => {
   return (
-    <ResponsiveContainer height="100%" width="100%">
-      <LineChart data={data} height={4000} width={5000}>
-        <Line type="monotone" dataKey="amt" />
-        <CartesianGrid stroke="#303030" strokeDasharray={'5 5'} />
-        <XAxis dataKey="name" />
-        <YAxis dataKey="uv" />
-        <Tooltip />
-        <Legend />
-      </LineChart>
-    </ResponsiveContainer>
+    <>
+      <p className="font-sans font-normal" style={{ fontSize: '20px' }}>
+        Month Name
+      </p>
+      <ResponsiveContainer height="100%" width="100%">
+        <LineChart data={data} height={4000} width={5000}>
+          <Line type="monotone" dataKey="amt" />
+
+          <CartesianGrid stroke="#303030" strokeDasharray={'5 5'} />
+          <XAxis dataKey="name" />
+          <YAxis dataKey="uv" />
+          <Tooltip />
+          <Legend />
+        </LineChart>
+      </ResponsiveContainer>
+      <div className="flex flex-row gap-2">
+        <button className="btn btn-circle btn-sm">
+          <IconArrowLeft />
+        </button>
+        <button className="btn btn-circle btn-sm">
+          <IconArrowRight />
+        </button>
+      </div>
+    </>
   )
 }
