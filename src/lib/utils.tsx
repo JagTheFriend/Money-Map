ab33cb8cec42e6d477806f7fde9e42aeeb13aecc
JagTheFriend <JagTheFriend12@gmail.com>
2024-11-14 08:40:46 +0530

    Created a util function to attach metadata to the transaction
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,6 +1,18 @@
+import type { Transaction } from '@prisma/client'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function attachMetadataToTransaction(transactions: Transaction[]) {
+  return transactions.map((transaction) => {
+    return {
+      ...transaction,
+      month: transaction.purchaseDate.getMonth(),
+      year: transaction.purchaseDate.getFullYear(),
+      dayName: transaction.purchaseDate.toString().split(' ')[0],
+    }
+  })
 }
