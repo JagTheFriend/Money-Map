f3c451bd579a5f38ea20a749545bcf56446d9eb5
JagTheFriend <JagTheFriend12@gmail.com>
2024-11-15 22:58:27 +0530

    Aggregate data before sending it
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,20 +1,30 @@
 import type { Transaction } from '@prisma/client'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import type { CustomTransactionType } from './types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export function attachMetadataToTransaction(transactions: Transaction[]) {
-  return transactions.map((transaction) => {
-    return {
-      ...transaction,
-      month: transaction.purchaseDate.getMonth(),
-      year: transaction.purchaseDate.getFullYear(),
-      dayName: transaction.purchaseDate.toLocaleString('en-us', {
-        weekday: 'long',
+  const data: CustomTransactionType[] = []
+
+  transactions.map((currentTransaction) => {
+    data.push({
+      monthName: currentTransaction.purchaseDate.toLocaleString('default', {
+        month: 'long',
       }),
-    }
+      totalAmount: parseInt(currentTransaction.amount),
+      transactions: transactions.filter(
+        (filterTransaction) =>
+          filterTransaction.purchaseDate.getMonth() ===
+          currentTransaction.purchaseDate.getMonth(),
+      ),
+    })
   })
+
+  console.log(data)
+
+  return data
 }
