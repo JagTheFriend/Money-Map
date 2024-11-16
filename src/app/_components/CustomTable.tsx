5f6962b312fd8dc8820fc0c88717f117d58b2885
JagTheFriend <JagTheFriend12@gmail.com>
2024-11-16 11:26:07 +0530

    Display total of the month along with the date the transaction occured
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/_components/CustomTable.tsx
+++ src/app/_components/CustomTable.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { motion } from 'framer-motion'
-import type { CustomTransactionType } from '~/lib/types'
+import { format } from "date-fns";
+import { motion } from 'framer-motion';
+import type { CustomTransactionType } from '~/lib/types';
 
 function TableContent({ transaction }: { transaction: CustomTransactionType }) {
   return (
@@ -13,13 +14,17 @@ function TableContent({ transaction }: { transaction: CustomTransactionType }) {
       </thead>
       <tbody>
         {transaction.transactions.map((monthTransaction) => {
-          return (
-            <tr key={monthTransaction.id}>
-              <td>{monthTransaction.title}</td>
-              <td>{monthTransaction.amount}</td>
-            </tr>
-          )
+          return <tr key={monthTransaction.id}>
+            <td>{format(monthTransaction.purchaseDate, "dd/MM/yyyy")}</td>
+            <td>{monthTransaction.title}</td>
+            <td>{monthTransaction.amount}</td>
+          </tr>
         })}
+        <tr className="text-gray-500">
+          <td></td>
+          <td>Total</td>
+          <td>{transaction.totalAmount}</td>
+        </tr>
       </tbody>
     </>
   )
